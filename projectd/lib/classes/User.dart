c76e2b7600d9a37a4bd8ac89f086e6fcd104938a
jonas de boer <jonasdeboer02@gmail.com>
2021-05-16 23:50:29 +0200

    Token is now read from YAML

--- projectd/lib/classes/User.dart
+++ projectd/lib/classes/User.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'dart:convert';
+import "package:yaml/yaml.dart";
+import "package:flutter/services.dart" as sv;
 
 class User {
-
   //User information that we need
   String name;
   String age;
@@ -13,47 +14,40 @@ class User {
   String restheartrateweek;
   String sleepscore;
 
-  User({this.name, this.age, this.gender, this.avatar, this.restheartrate, this.restheartrateweek, this.sleepscore});
+  User(
+      {this.name,
+      this.age,
+      this.gender,
+      this.avatar,
+      this.restheartrate,
+      this.restheartrateweek,
+      this.sleepscore});
 
   //Get userdata
   Future<void> GetData() async {
-    try{
+    try {
+      final temp = await sv.rootBundle.loadString("assets/test.yaml");
+      var temp2 = loadYaml(temp);
+      String bearer = temp2['AccountDetails']['Authorization'];
+      print(bearer);
       //Get profile data
-      Response response = await get(Uri.parse(
-          "https://api.fitbit.com/1/user/-/profile.json"
-      ),
-          headers: {
-            "Authorization":
-            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyM0I1NjUiLCJzdWIiOiI5QkNXQzYiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJhY3QgcnNldCBybG9jIHJ3ZWkgcmhyIHJudXQgcnBybyByc2xlIiwiZXhwIjoxNjUyNjUyOTA0LCJpYXQiOjE2MjExMTY5Njl9.EZXJUVHO6Oz-vtAPeKzJwziWvHUuXMIH0gF4BSqC1Jw"
-          });
+      Response response = await get(
+          Uri.parse("https://api.fitbit.com/1/user/-/profile.json"),
+          headers: {"Authorization": "Bearer $bearer"});
 
       //Get heartrate data
-      Response response2 = await get(Uri.parse(
-          "https://api.fitbit.com/1/user/-/activities/heart/date/today/7d.json"
-      ),
-          headers: {
-            "Authorization":
-            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyM0I1NjUiLCJzdWIiOiI5QkNXQzYiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJhY3QgcnNldCBybG9jIHJ3ZWkgcmhyIHJudXQgcnBybyByc2xlIiwiZXhwIjoxNjUyNjUyOTA0LCJpYXQiOjE2MjExMTY5Njl9.EZXJUVHO6Oz-vtAPeKzJwziWvHUuXMIH0gF4BSqC1Jw"
-          });
-
+      Response response2 = await get(
+          Uri.parse(
+              "https://api.fitbit.com/1/user/-/activities/heart/date/today/7d.json"),
+          headers: {"Authorization": "Bearer $bearer"});
       //Get sleepamount data
-      Response response3 = await get(Uri.parse(
-          "https://api.fitbit.com/1.2/user/-/sleep/date/today.json"
-      ),
-          headers: {
-            "Authorization":
-            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyM0I1NjUiLCJzdWIiOiI5QkNXQzYiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJhY3QgcnNldCBybG9jIHJ3ZWkgcmhyIHJudXQgcnBybyByc2xlIiwiZXhwIjoxNjUyNjUyOTA0LCJpYXQiOjE2MjExMTY5Njl9.EZXJUVHO6Oz-vtAPeKzJwziWvHUuXMIH0gF4BSqC1Jw"
-          });
-
+      Response response3 = await get(
+          Uri.parse("https://api.fitbit.com/1.2/user/-/sleep/date/today.json"),
+          headers: {"Authorization": "Bearer $bearer"});
       //Get sleepgoaldata
-      Response response4 = await get(Uri.parse(
-          "https://api.fitbit.com/1/user/-/sleep/goal.json"
-      ),
-          headers: {
-            "Authorization":
-            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyM0I1NjUiLCJzdWIiOiI5QkNXQzYiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJhY3QgcnNldCBybG9jIHJ3ZWkgcmhyIHJudXQgcnBybyByc2xlIiwiZXhwIjoxNjUyNjUyOTA0LCJpYXQiOjE2MjExMTY5Njl9.EZXJUVHO6Oz-vtAPeKzJwziWvHUuXMIH0gF4BSqC1Jw"
-          });
-
+      Response response4 = await get(
+          Uri.parse("https://api.fitbit.com/1/user/-/sleep/goal.json"),
+          headers: {"Authorization": "Bearer $bearer"});
       //Data in maps
       Map data = jsonDecode(response.body);
       Map heartratedata = jsonDecode(response2.body);
@@ -63,8 +57,12 @@ class User {
       //Calculate weekly resting heartrate
       int total = 0;
 
-      for (int i = 0; i<7; i++){
-        total += heartratedata['activities-heart'][i]['value']['restingHeartRate']!= null ? heartratedata['activities-heart'][i]['value']['restingHeartRate'] : 0;
+      for (int i = 0; i < 7; i++) {
+        total += heartratedata['activities-heart'][i]['value']
+                    ['restingHeartRate'] !=
+                null
+            ? heartratedata['activities-heart'][i]['value']['restingHeartRate']
+            : 0;
       }
 
       //Pass data to User class
@@ -72,20 +70,23 @@ class User {
       this.age = data['user']['age'].toString();
       this.gender = data['user']['gender'];
       this.avatar = data['user']['avatar150'];
-      this.restheartrate = heartratedata['activities-heart'][6]['value']['restingHeartRate'].toString();
+      this.restheartrate = heartratedata['activities-heart'][6]['value']
+              ['restingHeartRate']
+          .toString();
 
       //Calculate weekly resting heartrate
-      this.restheartrateweek = (total/7).toString().substring(0,2);
+      this.restheartrateweek = (total / 7).toString().substring(0, 2);
 
       //calculate sleepscore = actual sleep in hours / sleepgoal * 10. if sleeping more than goal aka score > 10 then sleepscore = 10 - (score-10)
-      double sleepgoalinhours = sleepgoaldata['goal']['minDuration']/60;
-      double sleepscore = sleepdata['sleep'].isNotEmpty ? sleepdata['sleep'][0]['duration']/3600000/sleepgoalinhours*10 : 0.0;
-      this.sleepscore = sleepscore < 10 ? sleepscore.toString().substring(0,3) : (10-(sleepscore-10)).toString().substring(0,3);
-    }
-    catch(e){
+      double sleepgoalinhours = sleepgoaldata['goal']['minDuration'] / 60;
+      double sleepscore = sleepdata['sleep'].isNotEmpty
+          ? sleepdata['sleep'][0]['duration'] / 3600000 / sleepgoalinhours * 10
+          : 0.0;
+      this.sleepscore = sleepscore < 10
+          ? sleepscore.toString().substring(0, 3)
+          : (10 - (sleepscore - 10)).toString().substring(0, 3);
+    } catch (e) {
       print(e);
     }
-
-
   }
 }
