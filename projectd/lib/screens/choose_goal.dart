c74d4e431a1d91902bda9ba27c7659841c08f259
jonas de boer <jonasdeboer02@gmail.com>
2021-05-26 22:13:45 +0200

    Inputfield for hours

--- projectd/lib/screens/choose_goal.dart
+++ projectd/lib/screens/choose_goal.dart
@@ -8,16 +8,122 @@ class ChooseGoalScreen extends StatefulWidget {
 }
 
 class _ChooseGoalScreenState extends State<ChooseGoalScreen> {
+  DateTime _date;
+  int _hoursAWeek;
+  String phase = "pickDate";
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text("Set Goal"),
-          centerTitle: true,
-          backgroundColor: Colors.blue[400],
-          elevation: 0.0,
+    if (phase == "pickDate") {
+      return Scaffold(
+          body: Column(
+        children: <Widget>[
+          Expanded(
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                Text(_date == null
+                    ? 'Please pick the date of the challenge'
+                    : "Date of challenge: ${_date.day}/${_date.month}/${_date.year}")
+              ])),
+          Expanded(
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                RaisedButton(
+                    child: Text('Pick a date'),
+                    onPressed: () {
+                      showDatePicker(
+                              context: context,
+                              initialDate: DateTime.now(),
+                              firstDate: DateTime(2001),
+                              lastDate: DateTime(2022))
+                          .then((date) {
+                        setState(() {
+                          _date = date;
+                        });
+                      });
+                    })
+              ])),
+          Expanded(
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  RaisedButton(
+                    child: Text('Back'),
+                    onPressed: () {
+                      Navigator.pushNamed(context, "/");
+                    },
+                  ),
+                  RaisedButton(
+                    child: Text('Next'),
+                    onPressed: () {
+                      if (_date != null) {
+                        phase = "pickHours";
+                        setState(() {});
+                      }
+                    },
+                  )
+                ]),
+          )
+        ],
+      ));
+    } else if (phase == "pickHours") {
+      return Scaffold(
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text("How many hours do you have a week?")
+                  ]),
+            ),
+            Expanded(
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                  Container(
+                      width: 100,
+                      child: TextField(
+                          onChanged: (newtext) {
+                            _hoursAWeek = int.parse(newtext);
+                          },
+                          textAlign: TextAlign.left,
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'enter hours',
+                            hintStyle: TextStyle(color: Colors.grey),
+                          )))
+                ])),
+            Expanded(
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    RaisedButton(
+                      child: Text('Back'),
+                      onPressed: () {
+                        if (_date != null) {
+                          phase = "pickDate";
+                          setState(() {});
+                        }
+                      },
+                    ),
+                    RaisedButton(
+                      child: Text('Finish'),
+                      onPressed: () {},
+                    )
+                  ]),
+            )
+          ],
         ),
-        body: Padding(padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0)));
+      );
+    }
   }
 }
